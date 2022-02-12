e3c90a7075b69b3f80baa60b95173994a2f2c63a
aaronchan233 <59012372+aaronchan233@users.noreply.github.com>
2022-02-12 12:11:42 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 var text = document.getElementById("text");
 var search = document.getElementById("search");
-var baidu = "https://www.baidu.com/#ie={inputEncoding}&wd=";
+var baidu = "http://www.baidu.com/s?wd=";
 text.onkeyup = function() {
     if (event.keyCode == 13) {
         window.open(baidu + text.value);
