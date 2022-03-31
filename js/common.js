edef5b43acd7989a6f2778e0fc80f8dea106f3d7
aaronchan233 <1992680764@qq.com>
2022-03-31 18:06:13 +0800

    first git

--- js/common.js
+++ js/common.js
@@ -1,13 +1,31 @@
-var text = document.getElementById("text");
-var search = document.getElementById("search");
+var hidden = document.querySelector("#close");
+
+var text = document.querySelector("#text");
+var search = document.querySelector("#search");
 var baidu = "http://www.baidu.com/s?wd=";
-text.onkeyup = function() {
-    if (event.keyCode == 13) {
+text.addEventListener("keyup", function(e) {
+    if (e.keyCode == 13) {
+        if (text.value == "o--o") {
+            hidden.id = "open";
+            text.value = "";
+        } else {
+            window.open(baidu + text.value);
+            text.value = "";
+        }
+    }
+});
+search.addEventListener("click", function() {
+    if (text.value == "o--o") {
+        hidden.id = "open";
+        text.value = "";
+    } else {
         window.open(baidu + text.value);
         text.value = "";
     }
-}
-search.onclick = function() {
-    window.open(baidu + text.value);
-    text.value = "";
-}
+});
+
+var mp3 = document.querySelector("audio");
+var logo = document.querySelector("h1");
+logo.addEventListener("click", function() {
+    mp3.play();
+});
